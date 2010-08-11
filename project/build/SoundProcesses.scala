09bd3e19cff687961b0bb0f48ebb89d355d32309
Sciss <contact@sciss.de>
2010-08-11 15:41:18 +0100

    ...

--- project/build/SoundProcesses.scala
+++ project/build/SoundProcesses.scala
@@ -10,15 +10,6 @@ class SoundProcessesProject( info: ProjectInfo ) extends DefaultProject( info ) 
    val scalaAudioFile   = "de.sciss" %% "scalaaudiofile" % "0.13"
    val scalaOSC         = "de.sciss" %% "scalaosc" % "0.19"
    val scalaCollider    = "de.sciss" %% "scalacollider" % "0.17"
-//   val dep9 = "Stanford CS - Pervasive Parallelism Laboratory" %% "ccstm" % "0.2"
    val ccstm            = "edu.stanford.ppl" % "ccstm" % "0.2.1-for-scala-2.8.0"
    val ccstmRepo        = "CCSTM Release Repository at PPL" at "http://ppl.stanford.edu/ccstm/repo-releases"
-//   // ---- ccstm dependancies ----
-//   val scalatest = crossScalaVersionString match {
-//     // RC7 is the same as the release, but scalatest against the release is not
-//     // yet available
-//     case "2.8.0" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC7-SNAPSHOT"
-//     case _ => "org.scalatest" % "scalatest" % ("1.2-for-scala-" + crossScalaVersionString + "-SNAPSHOT")
-//   }
-//   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
 }
