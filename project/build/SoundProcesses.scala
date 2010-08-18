f74be342104200919d6177f054787135293cc390
Sciss <contact@sciss.de>
2010-08-18 23:57:05 +0100

    ...

--- project/build/SoundProcesses.scala
+++ project/build/SoundProcesses.scala
@@ -5,7 +5,7 @@ import sbt.{ FileUtilities => FU, _}
  *    @version 0.15, 11-Aug-10
  */
 class SoundProcessesProject( info: ProjectInfo ) extends DefaultProject( info ) {
-   val scalaCollider    = "de.sciss" %% "scalacollider" % "0.17"
+   val scalaCollider    = "de.sciss" %% "scalacollider" % "0.18"
 //   val ccstm            = "edu.stanford.ppl" % "ccstm" % "0.2.1-for-scala-2.8.0"
 //   val ccstmRepo        = "CCSTM Release Repository at PPL" at "http://ppl.stanford.edu/ccstm/repo-releases"
    val ccstm = "edu.stanford.ppl" % "ccstm" % "0.2.2-for-scala-2.8.0-SNAPSHOT"
