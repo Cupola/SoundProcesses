61b3ac5fc61c12f4a7ba1d79ab25000ec31a2b68
Sciss <contact@sciss.de>
2010-08-18 20:02:26 +0100

    switching to newer ccstm snapshot

--- project/build/SoundProcesses.scala
+++ project/build/SoundProcesses.scala
@@ -6,6 +6,9 @@ import sbt.{ FileUtilities => FU, _}
  */
 class SoundProcessesProject( info: ProjectInfo ) extends DefaultProject( info ) {
    val scalaCollider    = "de.sciss" %% "scalacollider" % "0.17"
-   val ccstm            = "edu.stanford.ppl" % "ccstm" % "0.2.1-for-scala-2.8.0"
-   val ccstmRepo        = "CCSTM Release Repository at PPL" at "http://ppl.stanford.edu/ccstm/repo-releases"
+//   val ccstm            = "edu.stanford.ppl" % "ccstm" % "0.2.1-for-scala-2.8.0"
+//   val ccstmRepo        = "CCSTM Release Repository at PPL" at "http://ppl.stanford.edu/ccstm/repo-releases"
+   val ccstm = "edu.stanford.ppl" % "ccstm" % "0.2.2-for-scala-2.8.0-SNAPSHOT"
+   val ccstmRepo = "CCSTM Release Repository at PPL" at "http://ppl.stanford.edu/ccstm/repo-releases"
+   val ccstmSnap = "CCSTM Snapshot Repository at PPL" at "http://ppl.stanford.edu/ccstm/repo-snapshots"
 }
